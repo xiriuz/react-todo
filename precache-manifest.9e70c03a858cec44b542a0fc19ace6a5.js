746ee1faee18b84bc6be2a979e3280c98643f44f
KursNim <kursnim@gmail.com>
2020-08-29 16:38:24 +0900

    Updates

---
+++ precache-manifest.9e70c03a858cec44b542a0fc19ace6a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d6099eb1e1bd2c5f992c6af2ef5d0e2",
+    "url": "/react-todo.git/index.html"
+  },
+  {
+    "revision": "96b50abd9d0f724450bf",
+    "url": "/react-todo.git/static/css/main.4d8ab585.chunk.css"
+  },
+  {
+    "revision": "9968bcf772b207fb7481",
+    "url": "/react-todo.git/static/js/2.0e57aa0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo.git/static/js/2.0e57aa0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96b50abd9d0f724450bf",
+    "url": "/react-todo.git/static/js/main.d909aea3.chunk.js"
+  },
+  {
+    "revision": "b06953b83cf267952b70",
+    "url": "/react-todo.git/static/js/runtime-main.f8220130.js"
+  }
+]);
